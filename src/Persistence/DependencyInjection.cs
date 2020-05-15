0b4b3e97ed1d4bb2d815ee647e7cd5c2f1413009
dirocchini <roquinni@gmail.com>
2020-05-15 10:47:24 -0300

    added in memory database support

--- src/Persistence/DependencyInjection.cs
+++ src/Persistence/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
 using Shared.Support.Configuration;
 
 namespace Persistence
@@ -13,9 +15,12 @@ namespace Persistence
 
             services.Configure<DbConnectionConfig>(options => configuration.GetSection("DbConnection").Bind(options));
 
-            services.AddDbContext<ApplicationContextSqlServer>();
+            //services.AddDbContext<ApplicationContextSqlServer>();
+            //services.AddScoped<IApplicationContext>(provider => provider.GetService<ApplicationContextSqlServer>());
 
-            services.AddScoped<IApplicationContext>(provider => provider.GetService<ApplicationContextSqlServer>());
+
+            services.AddScoped<IApplicationContext>(provider => provider.GetService<ApplicationContextInMemory>());
+            services.AddDbContext<ApplicationContextInMemory>(options => options.UseInMemoryDatabase("InMemoryDatabase"));
 
             return services;
         }
